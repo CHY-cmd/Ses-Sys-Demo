69e83b3dba750035b8425e17ec8defa4ffb9cd41
CHY-cmd <yc_sgchy@qq.com>
2024-11-19 16:38:05 +0800

    cloud

--- backend/DemoSys/src/main/java/com/itec/DemoSysApplication.java
+++ backend/DemoSys/src/main/java/com/itec/DemoSysApplication.java
@@ -1,9 +1,11 @@
 package com.itec;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.itec.mapper")
 public class DemoSysApplication {
 
 	public static void main(String[] args) {
